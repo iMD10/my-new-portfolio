3f00bcf60d94f0e5865069a649ceb66f7555719c
Muhannad <mohanad.alfawzan1@gmail.com>
2026-06-15 18:48:37 +0300

    ph

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import posthog from 'posthog-js'
 import App from './App'
 import './index.css'
+
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_POSTHOG_HOST ?? 'https://us.i.posthog.com',
+  person_profiles: 'identified_only',
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
